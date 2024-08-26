014b2286b8c7f8451cd9b663ce5bb1ca0d39aaf0
MarsiPeto <marcelina.peto09@gmail.com>
2024-08-26 23:48:33 +0100

    Plotesimi me metoda i AuthorRepository  dhe AuthorService

--- src/main/java/org/example/author/AuthorRepository.java
+++ src/main/java/org/example/author/AuthorRepository.java
@@ -1,4 +1,90 @@
 package org.example.author;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import java.lang.reflect.Executable;
+import java.util.*;
+
 public class AuthorRepository {
+
+    private SessionFactory sessionFactory;
+
+    public AuthorRepository(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public void addAuthor(AuthorEntity author) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.save(author);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    public void removeAuthor(UUID id) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            AuthorEntity author = session.createQuery("Select s From AuthorEntity s where id =:id", AuthorEntity.class)
+                    .setParameter("id", id)
+                    .getSingleResult();
+            session.delete(author);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    public AuthorEntity findById(UUID id) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            AuthorEntity author = session.createQuery("Select s From AuthorEntity s where id = :id", AuthorEntity.class)
+                    .setParameter("id", id)
+                    .getSingleResult();
+            transaction.commit();
+            return author;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+
+    public List<AuthorEntity> findAll() {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            List<AuthorEntity> list = session.createQuery("Select s From AuthorEntity s", AuthorEntity.class)
+                    .getResultList();
+            transaction.commit();
+            return list;
+        } catch (Exception e) {
+
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            throw e;
+
+        }
+
+
+    }
 }
--- src/main/java/org/example/author/AuthorService.java
+++ src/main/java/org/example/author/AuthorService.java
@@ -1,4 +1,27 @@
 package org.example.author;
 
+import java.util.*;
+
 public class AuthorService {
+    private AuthorRepository authorRepository;
+
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    public void addAuthor(AuthorEntity author){
+        authorRepository.addAuthor(author);
+    }
+
+    public void removeAuthor(UUID id){
+        authorRepository.removeAuthor(id);
+    }
+
+    public AuthorEntity findById(UUID id){
+        return authorRepository.findById(id);
+    }
+
+    public List<AuthorEntity> findAll(){
+        return authorRepository.findAll();
+    }
 }
