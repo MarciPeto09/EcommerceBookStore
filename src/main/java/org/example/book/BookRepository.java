8631f8ab6dadfec4bf36d36918906ef5ef4d676e
MarsiPeto <marcelina.peto09@gmail.com>
2024-08-28 09:18:01 +0100

    Plotesii mme metoda i BookRepository dhe BookService

--- src/main/java/org/example/book/BookRepository.java
+++ src/main/java/org/example/book/BookRepository.java
@@ -1,4 +1,93 @@
 package org.example.book;
 
+import org.example.author.AuthorEntity;
+import org.hibernate.*;
+import org.hibernate.Transaction;
+
+import java.util.List;
+import java.util.UUID;
+
 public class BookRepository {
+
+    private SessionFactory sessionFactory;
+
+    public BookRepository(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public void addBook(BookEntity book){
+        Transaction transaction = null;
+        try(Session session = sessionFactory.openSession()){
+            transaction = session.beginTransaction();
+            session.save(book);
+            transaction.commit();
+        }catch(Exception e){
+            if(transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public void removeBook(UUID id) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            AuthorEntity author = session.createQuery("Select s From AuthorEntity s where id =:id", AuthorEntity.class)
+                    .setParameter("id", id)
+                    .getSingleResult();
+            session.delete(author);
+            transaction.commit();
+        }catch (Exception e){
+            if(transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public BookEntity findById(UUID id){
+        BookEntity book = null;
+        try(Session session = sessionFactory.openSession()){
+             book = session.createQuery("Select s From BookEntity s where id =:id", BookEntity.class)
+                    .setParameter("id", id)
+                    .getSingleResult();
+
+        }catch(Exception e ){
+            e.printStackTrace();
+        }
+        return book;
+    }
+
+    public List<BookEntity> findAll(){
+        List<BookEntity> lista = null;
+        try(Session session = sessionFactory.openSession()){
+            lista = session.createQuery("Select s From BookEntity s",BookEntity.class).getResultList();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return lista;
+    }
+
+
+    public void updateBook(UUID id,BookEntity book){
+        Transaction transaction = null;
+        BookEntity existingBook = null;
+        try(Session session = sessionFactory.openSession()){
+            transaction = session.beginTransaction();
+            existingBook.setTitle(book.getTitle());
+            existingBook.setPrice(book.getPrice());
+            existingBook.setListOfAuthorsXBook(book.getListOfAuthorsXBook());
+            existingBook.setOrder(book.getOrder());
+            existingBook.setCategory(book.getCategory());
+            session.update(existingBook);
+            transaction.commit();
+        }catch(Exception e){
+            if(transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/org/example/book/BookService.java
+++ src/main/java/org/example/book/BookService.java
@@ -1,4 +1,34 @@
 package org.example.book;
 
+import java.awt.print.Book;
+import java.util.List;
+import java.util.UUID;
+
 public class BookService {
+
+    private BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook(BookEntity book){
+        bookRepository.addBook(book);
+    }
+
+    public void removeBook(UUID id){
+        bookRepository.removeBook(id);
+    }
+
+    public BookEntity findById(UUID id){
+        return bookRepository.findById(id);
+    }
+
+    public List<BookEntity> findAll(){
+        return bookRepository.findAll();
+    }
+
+    public void upDate(UUID id, BookEntity book){
+        bookRepository.updateBook(id, book);
+    }
 }
