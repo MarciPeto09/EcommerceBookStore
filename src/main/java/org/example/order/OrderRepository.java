074d6fa0bc038dc6a40067b48f8b2ff8aa8bd2b2
MarsiPeto <marcelina.peto09@gmail.com>
2024-08-28 11:54:45 +0100

    Plotesimi i klasave OrderRepository dhe OrderService

--- src/main/java/org/example/order/OrderRepository.java
+++ src/main/java/org/example/order/OrderRepository.java
@@ -1,4 +1,93 @@
 package org.example.order;
 
+import org.example.category.CategoryEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import java.util.List;
+import java.util.UUID;
+
 public class OrderRepository {
+    private SessionFactory sessionFactory;
+
+    public OrderRepository(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public void addOrder(OrderEntity order) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.save(order);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void removeOrder(UUID id) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            OrderEntity order = session.createQuery("Select s From OrderEntity s where id = :id", OrderEntity.class)
+                    .setParameter("id", id)
+                    .getSingleResult();
+            session.delete(order);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+
+    public OrderEntity findById(UUID id) {
+        OrderEntity order = null;
+        try (Session session = sessionFactory.openSession()) {
+            order = session.createQuery("Select s From CategoryEntity s  where id = :id", OrderEntity.class)
+                    .setParameter("id", id)
+                    .getSingleResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return order;
+    }
+
+
+    public List<OrderEntity> findAll() {
+        List<OrderEntity> list = null;
+        try (Session session = sessionFactory.openSession()) {
+            list = session.createQuery("Select s From CategoryEntity s", OrderEntity.class)
+                    .getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public void upDate(UUID id, OrderEntity order) {
+        Transaction transaction = null;
+        OrderEntity existingOrder = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            existingOrder = session.createQuery("Select s From CategoryEntity s where id =:id", OrderEntity.class)
+                    .setParameter("id", id)
+                    .getSingleResult();
+            existingOrder.setAdress(order.getAdress());
+            existingOrder.setData(order.getData());
+            existingOrder.setUser(order.getUser());
+            existingOrder.setBooks(order.getBooks());
+            existingOrder.setStatus(order.getStatus());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/org/example/order/OrderService.java
+++ src/main/java/org/example/order/OrderService.java
@@ -1,4 +1,35 @@
 package org.example.order;
 
+import org.example.category.CategoryEntity;
+import org.example.category.CategoryRepository;
+
+import java.util.List;
+import java.util.UUID;
+
 public class OrderService {
+    private OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public void addOrder(OrderEntity order){
+        orderRepository.addOrder(order);
+    }
+
+    public void removeOrder(UUID id){
+        orderRepository.removeOrder(id);
+    }
+
+    public OrderEntity findById(UUID id){
+        return orderRepository.findById(id);
+    }
+
+    public List<OrderEntity> findAll(){
+        return orderRepository.findAll();
+    }
+
+    public void upDate(UUID id, OrderEntity order){
+        orderRepository.upDate(id, order);
+    }
 }
